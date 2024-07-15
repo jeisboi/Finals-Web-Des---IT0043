0b80399e2b0b35648cabe9923328ee9ce1ee8d99
jeisboi <202312310@fit.edu.ph>
2024-07-16 00:23:10 +0800

    antok na

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
+
+
 var tablinks = document.getElementsByClassName("tab-links");
 var tabcontents = document.getElementsByClassName("tab-contents");
+
 function opentab(tabname){
     for(tablink of tablinks){
             tablink.classList.remove("active-link");
@@ -8,4 +11,6 @@ function opentab(tabname){
     for(tabcontent of tabcontents){
         tabcontent.classList.remove("active-tab");
     }
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab")
 }
