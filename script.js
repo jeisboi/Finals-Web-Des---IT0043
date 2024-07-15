0747f9b061e62033c39fc5c1fbbfdb1a46d6af32
jeisboi <202312310@fit.edu.ph>
2024-07-16 02:56:40 +0800

    good night

--- script.js
+++ script.js
@@ -1,4 +1,25 @@
+// ==scroll up==
 
+const btn = document.querySelector(".jump-to-top");
+
+window.addEventListener("scroll", () => {
+    if(scrollY>250){
+        btn.classList.add('show')
+    }
+    else{
+        btn.classList.remove('show')
+    }
+})
+
+btn.addEventListener("click",()=>{
+    window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior:"smooth"
+    })
+})
+
+// == about me ==
 
 var tablinks = document.getElementsByClassName("tab-links");
 var tabcontents = document.getElementsByClassName("tab-contents");
