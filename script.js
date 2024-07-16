003657b05aa9738e7fba741d4806f2158bb1088e
jeisboi <202312310@fit.edu.ph>
2024-07-16 22:49:56 +0800

    FINAL COMMIT!

--- script.js
+++ script.js
@@ -35,3 +35,22 @@ function opentab(tabname){
     event.currentTarget.classList.add("active-link");
     document.getElementById(tabname).classList.add("active-tab")
 }
+// ====response====
+
+
+  const scriptURL = 'https://script.google.com/macros/s/AKfycbz1MUojUiVdErZwVQkF1VtS2MnywyPLcdLRQApUKXKkRJISUbrwm4nOIG1HDdUmvPdokA/exec'
+  const form = document.forms['submit-to-google-sheet']
+  const msg = document.getElementById("msg")
+
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response => {
+        msg.innerHTML = "Message already sent to author!"
+        setTimeout(function(){
+            msg.innerHTML = ""
+        }, 5000)
+        form.reset()
+      })
+      .catch(error => console.error('Error!', error.message))
+  })
